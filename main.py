df5d61113c11ef10b93fceefe125cfbf6f1280b8
N103Gremillion <nathan103grem@gmail.com>
2025-09-28 16:40:34 -0500

    some of tahoe

--- main.py
+++ main.py
@@ -4,62 +4,119 @@
 # TCP Tahoe Simulation
 import csv
 import random
+from typing import Iterator, List, TextIO
 
 # indexes of each value when reading from the input csv
-NO_INDEX = 0
-TIME_INDEX = 1
-SOURCE_INDEX = 2
-DESTINATION_INDEX = 3
-BYTES_IN_FLIGHT_INDEX = 6
-TCP_DELTA_INDEX = 7
-TCP_SEG_LEN_INDEX = 8
-CALCED_WINDOW_SIZE_INDEX = 9
-INFO_INDEX = 10
+NO_INDEX : int = 0
+TIME_INDEX : int = 1
+SOURCE_INDEX : int = 2
+DESTINATION_INDEX : int = 3
+BYTES_IN_FLIGHT_INDEX : int = 6
+TCP_DELTA_INDEX : int = 7
+TCP_SEG_LEN_INDEX : int = 8
+CALCED_WINDOW_SIZE_INDEX : int = 9
+INFO_INDEX : int = 10
+PHASE_INDEX : int = 11
 
 # file constants
-READ_FILE_NAME = "project1_data.csv"
-READ_FILE = open(READ_FILE_NAME, "r") 
-FILE_READER = csv.reader(READ_FILE)
+READ_FILE_NAME : str = "input-data.csv"
+READ_FILE : TextIO = open(READ_FILE_NAME, "r") 
+FILE_READER : Iterator[List[str]] = csv.reader(READ_FILE)
 
-RENO_FILE = open("Reno.csv", "w", newline='')
-RENO_WRITER = csv.writer(RENO_FILE)
+RENO_FILE : TextIO = open("reno.csv", "w", newline='')
+RENO_WRITER : csv.writer = csv.writer(RENO_FILE)
 
-TAHOE_FILE = open("Tahoe.csv", "w", newline='')
-TAHOE_WRITER = csv.writer(TAHOE_FILE)
+TAHOE_FILE : TextIO = open("tahoe.csv", "w", newline='')
+TAHOE_WRITER : csv.writer = csv.writer(TAHOE_FILE)
 
 # ips
-CLIENT_IP = "192.168.10.184"
-SERVER_IP = "192.168.10.196"
+CLIENT_IP : str = "192.168.10.184"
+SERVER_IP : str = "192.168.10.196"
 
 # others
-ROWS = list(FILE_READER)
-MAX_ROW = len(ROWS)
+ROWS : List[List[str]] = list(FILE_READER)
+MAX_ROW : int = len(ROWS)
 
-def simulateReno(starting_index):
-    index = starting_index
+MSS : int = 1460
+CWND_BEFORE_CONGESTION : int = 262144 # I pulled this off the trace file
+
+# def simulateReno(starting_index : int):
     
+#     ssthresh = CWND_BEFORE_CONGESTION // 2
+
+#     index : int = starting_index
+    
+#     while index < MAX_ROW:
+#         index+=1
+        
+#     return
+
+def simulateTahoe(starting_index : int):
+    ssthreshold : int = CWND_BEFORE_CONGESTION // 2
+    cwnd : int = MSS # starts at 1 for Tahoe
+    time: float = float(ROWS[starting_index][TIME_INDEX])
+    index : int = starting_index
+    phase : str = "SS"
+    
+    # first implement slow start phase
     while index < MAX_ROW:
+        row : List[str] = ROWS[index]
+        
+        # Decide phase
+        if cwnd >= ssthreshold:
+            phase = "CA"
         index+=1
         
-    return
-
-def simulateTahoe(starting_index):
-    index = starting_index
-
-    while index < MAX_ROW:
-        index+=1
+        if phase == "SS":
+            cwnd += MSS
+        else:
+            cwnd += MSS * MSS // cwnd
+        
+        new_row = [
+        row[NO_INDEX],
+        row[TIME_INDEX],
+        row[TCP_DELTA_INDEX],
+        row[SOURCE_INDEX],
+        row[DESTINATION_INDEX],
+        row[TCP_SEG_LEN_INDEX],
+        row[BYTES_IN_FLIGHT_INDEX],
+        row[CALCED_WINDOW_SIZE_INDEX],
+        row[INFO_INDEX],
+        phase
+        ]
+        
+        TAHOE_WRITER.writerow(new_row)
+            
     return 
 
+# add the phase column
+ROWS[0].append("Phase") 
+for row in ROWS[1:]:
+    row.append("---")
     
 ### Open csv file, extract all rows, create new csv file and edit all rows from 3503 on ###
 for i, row in enumerate(ROWS):
+    
+    new_row : List[str] = [
+        row[NO_INDEX],
+        row[TIME_INDEX],
+        row[TCP_DELTA_INDEX],
+        row[SOURCE_INDEX],
+        row[DESTINATION_INDEX],
+        row[TCP_SEG_LEN_INDEX],
+        row[BYTES_IN_FLIGHT_INDEX],
+        row[CALCED_WINDOW_SIZE_INDEX],
+        row[INFO_INDEX],
+        row[PHASE_INDEX]
+    ]
+    
     if row[0] == "3503":
         simulateTahoe(i)
-        simulateReno(i)
+        # simulateReno(i)
         break # INTEGERAL LINE, DO NOT REMOVE!!!
     else:
-        RENO_WRITER.writerows(row)
-        TAHOE_WRITER.writerows(row)
+        RENO_WRITER.writerow(new_row)
+        TAHOE_WRITER.writerow(new_row)
             
 READ_FILE.close()
 RENO_FILE.close()
